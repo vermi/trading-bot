bd00ec8eace722af6199c95a623a639eddb161d5
Justin Vermillion <justin@lockboxsecurity.net>
2021-01-27 17:56:25 -0700

    complete refactor of getData.py

--- getData.py
+++ getData.py
@@ -1,5 +1,4 @@
 import string
-import time
 from datetime import datetime
 
 import numpy as np
@@ -7,128 +6,207 @@ import pandas as pd
 import pytz
 import requests
 from bs4 import BeautifulSoup
-
-# import pyarrow
 from google.cloud import bigquery, storage
 
+DEBUG = False
+# Cloud functions use UTC so we convert to EST
+today = datetime.today().astimezone(pytz.timezone("America/New_York"))
+today_fmt = today.strftime("%Y-%m-%d")
 
-def daily_equity_quotes(event, context):
-    # Get the api key from cloud storage
+
+def fetch_api_key(bucket_name="fair-sandbox", file_name="td-key") -> str:
+    """
+    Retrieves the TDA API key from Google Cloud Storage
+
+        Parameters:
+            bucket_name (str): the name of the bucket containing the key file
+            file_name (str): the name of the file containing the key
+
+        Returns:
+            api_key (str): The TDA API key
+    """
     storage_client = storage.Client()
-    bucket = storage_client.get_bucket("fair-sandbox")
-    blob = bucket.blob("td-key")
+    bucket = storage_client.get_bucket(bucket_name)
+    blob = bucket.blob(file_name)
     api_key = blob.download_as_string()
 
-    # Check if the market was open today. Cloud functions use UTC so we convert to EST
-    today = datetime.today().astimezone(pytz.timezone("America/New_York"))
-    today_fmt = today.strftime("%Y-%m-%d")
+    return api_key
 
-    # Call the td ameritrade hours endpoint for equities to see if it is open
+
+def request_quote(symbol, api_key) -> pd.DataFrame:
+    """
+    Retrieves quote data for a list of symbols via the TDA API.
+
+        Parameters:
+            symbol (list): A list of symbols to quote
+            api_key (str): The TDA API credentials
+
+        Returns:
+            df_quotes (pd.DataFrame): A DataFrame containing the requested quotes.
+    """
+    url = r"https://api.tdameritrade.com/v1/marketdata/quotes"
+
+    params = {
+        "apikey": api_key,
+        "symbol": symbol,
+    }
+
+    request = requests.get(url=url, params=params).json()
+    df_quotes = pd.DataFrame.from_dict(request, orient="index").reset_index(drop=True)
+
+    return df_quotes
+
+
+def check_open() -> bool:
+    """
+    Returns the market open status for the day.
+
+        Returns:
+            is_open (bool): Boolean representing open status
+    """
+    # Call the TDA Hours endpoint for equities to see if it is open
     market_url = "https://api.tdameritrade.com/v1/marketdata/EQUITY/hours"
 
-    params = {"apikey": api_key, "date": today_fmt}
+    params = {
+        "apikey": fetch_api_key(),
+        "date": today_fmt,
+    }
 
     request = requests.get(url=market_url, params=params).json()
+    is_open = request["equity"]["EQ"]["isOpen"]
 
+    return is_open
+
+
+def chunks(full_list, n):
+    """
+    Breaks a list down into manageable chunks.
+
+        Parameters:
+            full_list (list): the list that needs to be chunked
+            n (int): max number of items per chunk
+
+        Returns:
+            chunk_list (list): a list of lists containing n items each
+    """
+    n = max(1, n)
+    chunk_list = list((full_list[i : (i + n)] for i in range(0, len(full_list), n)))
+
+    return chunk_list
+
+
+def fetch_symbols() -> list:
+    """
+    Retrieves the NYSE symbols currently available for trade.
+
+        Returns:
+            symbols_clean (list): A list of symbols in a readable format
+    """
+    alpha = list(string.ascii_uppercase)
+
+    symbols = []
+
+    # Loop through the letters in the alphabet to get the stocks on each page
+    # from the table and store them in a list
+    for each in alpha:
+        url = "http://eoddata.com/stocklist/NYSE/{}.htm".format(each)
+        soup = BeautifulSoup(requests.get(url).content, "html.parser")
+        table = soup.find("table", {"class": "quotes"})
+        for row in table.findAll("tr")[1:]:
+            symbols.append(row.findAll("td")[0].text.rstrip())
+
+    # Remove any trailing letters or punctuation
+    symbols_clean = []
+
+    for each in symbols:
+        each = each.replace(".", "-")
+        symbols_clean.append((each.split("-")[0]))
+
+    # use set() to remove duplicates
+    return list(set(symbols_clean))
+
+
+def get_data() -> pd.DataFrame:
+    """
+    Gathers quotes for all symbols and concatenates into a single DataFrame.
+
+        Returns:
+            df (pd.DataFrame): The aggregated quote data
+    """
+    symbols = fetch_symbols()
+
+    # The TD Ameritrade api has a limit to the number of symbols you can get data for
+    # in a single call so we chunk the list into 200 symbols at a time
+    symbols_chunked = chunks(symbols, 200)
+
+    # Iterate through symbols and request market data.
+    df = pd.concat(
+        [request_quote(sym, fetch_api_key()) for sym in symbols_chunked], sort=False,
+    )
+
+    return df
+
+
+def load_data(df, dataset="equity_data", table="daily_quote_data"):
+    """
+    Loads quote data into BigQuery or saves as CSV (debug mode).
+
+        Parameters:
+            df (pd.DataFrame): the DataFrame containing quote data to load
+            dataset (str): the BigQuery dataset
+            table (str): the BigQuery table to append data
+    """
+    # Add the date and format for BigQuery
+    df["date"] = pd.to_datetime(today_fmt)
+    df["date"] = df["date"].dt.date
+    df["divDate"] = pd.to_datetime(df["divDate"])
+    df["divDate"] = df["divDate"].dt.date
+    df["divDate"] = df["divDate"].fillna(np.nan)
+
+    # Remove anything without a price
+    df = df.loc[df["bidPrice"] > 0]
+
+    # Rename columns and format for BQ (can't start with a number)
+    df = df.rename(columns={"52WkHigh": "_52WkHigh", "52WkLow": "_52WkLow"})
+
+    if not DEBUG:
+        # Add to BigQuery
+        client = bigquery.Client()
+
+        dataset_ref = client.dataset(dataset)
+        table_ref = dataset_ref.table(table)
+
+        job_config = bigquery.LoadJobConfig()
+        job_config.source_format = bigquery.SourceFormat.CSV
+        job_config.autodetect = True
+        job_config.ignore_unknown_values = True
+        job = client.load_table_from_dataframe(
+            df, table_ref, location="US", job_config=job_config
+        )
+
+        job.result()
+    else:
+        # Otherwise save to CSV for testing purposes
+        print("Debug mode. Saving to CSV.")
+        df.to_csv(r"./debug-data.csv")
+
+
+def main(event, context) -> str:
+    """
+    This script is used to fetch daily price data for all NYSE ticker symbols.
+
+        Returns:
+            status (str): A status message for the Google Cloud Platform run log
+    """
     try:
-        if request["equity"]["EQ"]["isOpen"] is True:
-            # Get a current list of all the stock symbols for the NYSE
-            # Create a list of every letter in the alphabet
-            # Each page has a letter for all those symbols
-            # i.e. http://eoddata.com/stocklist/NYSE/A.htm'
-            alpha = list(string.ascii_uppercase)
-
-            symbols = []
-
-            # Loop through the letters in the alphabet to get the stocks on each page
-            # from the table and store them in a list
-            for each in alpha:
-                url = "http://eoddata.com/stocklist/NYSE/{}.htm".format(each)
-                resp = requests.get(url)
-                site = resp.content
-                soup = BeautifulSoup(site, "html.parser")
-                table = soup.find("table", {"class": "quotes"})
-                for row in table.findAll("tr")[1:]:
-                    symbols.append(row.findAll("td")[0].text.rstrip())
-
-            # Remove the extra letters on the end
-            symbols_clean = []
-
-            for each in symbols:
-                each = each.replace(".", "-")
-                symbols_clean.append((each.split("-")[0]))
-
-            # The TD Ameritrade api has a limit to the number of symbols you can get data for
-            # in a single call so we chunk the list into 200 symbols at a time
-            def chunks(l, n):
-                """
-                Takes in a list and how long you want
-                each chunk to be
-                """
-                n = max(1, n)
-                return (l[i : (i + n)] for i in range(0, len(l), n))
-
-            symbols_chunked = list(chunks(list(set(symbols_clean)), 200))
-
-            # Function for the api request to get the data from td ameritrade
-            def quotes_request(stocks):
-                """
-                Makes an api call for a list of stock symbols
-                and returns a dataframe
-                """
-                url = r"https://api.tdameritrade.com/v1/marketdata/quotes"
-
-                params = {"apikey": api_key, "symbol": stocks}
-
-                request = requests.get(url=url, params=params).json()
-
-                time.sleep(1)
-
-                return pd.DataFrame.from_dict(request, orient="index").reset_index(
-                    drop=True
-                )
-
-            # Loop through the chunked list of synbols
-            # and call the api. Append all the resulting dataframes into one
-            df = pd.concat([quotes_request(each) for each in symbols_chunked])
-
-            # Add the date and fmt the dates for BQ
-            df["date"] = pd.to_datetime(today_fmt)
-            df["date"] = df["date"].dt.date
-            df["divDate"] = pd.to_datetime(df["divDate"])
-            df["divDate"] = df["divDate"].dt.date
-            df["divDate"] = df["divDate"].fillna(np.nan)
-
-            # Remove anything without a price
-            df = df.loc[df["bidPrice"] > 0]
-
-            # Rename columns and format for bq (can't start with a number)
-            df = df.rename(columns={"52WkHigh": "_52WkHigh", "52WkLow": "_52WkLow"})
-
-            # Add to bigquery
-            client = bigquery.Client()
-
-            dataset_id = "equity_data"
-            table_id = "daily_quote_data"
-
-            dataset_ref = client.dataset(dataset_id)
-            table_ref = dataset_ref.table(table_id)
-
-            job_config = bigquery.LoadJobConfig()
-            job_config.source_format = bigquery.SourceFormat.CSV
-            job_config.autodetect = True
-            job_config.ignore_unknown_values = True
-            job = client.load_table_from_dataframe(
-                df, table_ref, location="US", job_config=job_config
-            )
-
-            job.result()
-
-            return "Success"
-
+        if check_open():
+            load_data(get_data())
+            return "Data load completed successfully."
         else:
-            # Market Not Open Today
-            pass
+            return "No data loaded: markets closed."
     except KeyError:
-        # Not a weekday
-        pass
+        return "No data loaded: markets closed."
+
+
+if __name__ == "__main__":
+    main(None, None)
